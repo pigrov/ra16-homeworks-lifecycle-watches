4338a8819a91bcbc92f7eaeb8249ef2f8a992e61
Dmitry Pigrov <pigrov@gmail.com>
2022-10-14 22:43:40 +0300

    Watches Update

--- src/components/Watches.js
+++ src/components/Watches.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 export default function Watches(props) {
   return (
-    <div className="d-flex justify-content-around">
-      <Card style={{ width: "18rem" }}>
-        {props.children}
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>{props.text}</Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>TimeZone</Form.Label>
+        <Form.Control type="text" placeholder="Enter TimeZone" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
