3a4488449f56631b979244543ebdeb2203b14398
Dmitry Pigrov <pigrov@gmail.com>
2022-10-14 22:47:41 +0300

    add uuid

--- src/components/Watches.js
+++ src/components/Watches.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
+import uuid from "react-uuid";
 
 export default function Watches(props) {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const data = {
+      id: uuid(),
+      date: evt.target.date.value,
+      count: evt.target.count.value,
+    };
+    console.log(data);
+    props.onSave(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Name</Form.Label>
         <Form.Control type="text" placeholder="Enter Name" />
