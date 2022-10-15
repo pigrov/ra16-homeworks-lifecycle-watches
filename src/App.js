8de3c211208ed15d7d18a180780137131b22d3d5
Dmitry Pigrov <pigrov@gmail.com>
2022-10-15 11:26:40 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import Watches from "./components/Watches";
+import uuid from "react-uuid";
 
 function App() {
-  return <Watches />;
+  const data = [
+    {
+      id: uuid(),
+      city: "Moscow",
+      time: new Date().toLocaleTimeString(),
+    },
+  ];
+
+  return <Watches data={data} />;
 }
 
 export default App;
--- src/components/Watches.js
+++ src/components/Watches.js
@@ -1,33 +1,69 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Alert, Form, Button } from "react-bootstrap";
 import uuid from "react-uuid";
 
 export default function Watches(props) {
+  const [data, setData] = useState(props.data);
+
+  console.log(data);
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const data = {
       id: uuid(),
-      date: evt.target.date.value,
-      count: evt.target.count.value,
+      city: evt.target.city.value,
+      time: evt.target.time.value,
     };
     console.log(data);
-    props.onSave(data);
+    handleSave(data);
   };
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Name" />
-      </Form.Group>
+  const handleSave = (value) => {
+    if (value.city && value.time) {
+      setData((prevState) => [...prevState, value]);
+    }
+  };
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>TimeZone</Form.Label>
-        <Form.Control type="text" placeholder="Enter TimeZone" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+  const listing = data.map((item) => {
+    return (
+      <Alert id={item.id}>
+        <Alert.Heading>{item.time}</Alert.Heading>
+        <hr />
+        <p className="mb-0">{item.city}</p>
+      </Alert>
+    );
+  });
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="city">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type={"text"}
+            defaultValue={"Moscow"}
+            placeholder={"Enter City"}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="time">
+          <Form.Label>TimeZone</Form.Label>
+          <Form.Control
+            type={"number"}
+            min={-12}
+            max={12}
+            step={1}
+            maxLength={2}
+            defaultValue={3}
+            placeholder={"Enter Time"}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <hr />
+      {listing}
+    </Container>
   );
 }
